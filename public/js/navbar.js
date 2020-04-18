6ff72f7f336a0d722ec53c3d3247da6f3ea5e692
Tuyama <ftuyama@gmail.com>
2020-04-18 15:16:01 +0100

    Add assets

---
+++ public/js/navbar.js
@@ -0,0 +1,38 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').animate({ top: '-69px' }, 'easeInOutCubic');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+        $('nav').animate({ top: '0px' }, 'easeInOutCubic');
+        }
+    }
+
+    lastScrollTop = st;
+}
