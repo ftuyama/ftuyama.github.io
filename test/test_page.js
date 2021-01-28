0db2782686ca836ff64f76c20e12e3005676de65
Felipe Tuyama <felipetuyama@gmail.com>
2021-01-28 02:53:25 +0000

    Update test_page.js

--- test/test_page.js
+++ test/test_page.js
@@ -29,7 +29,7 @@ describe('Page', () => {
 
     it('should have target blank', () => {
       links.each((i, link) => {
-        if (!link.attribs.href.match(/.*(resume|infinitusgo|google|#).*/)) {
+        if (!link.attribs.href.match(/.*(resume|infinitusgo|google|twitter|#).*/)) {
           // console.log(link.attribs.href);
           assert.equal('_blank', link.attribs.target, `${link.attribs.href} should have target _blank`);
         }
@@ -38,7 +38,7 @@ describe('Page', () => {
 
     it('should be valid', () => {
       links.each((i, link) => {
-        if (!link.attribs.href.match(/.*(resume|google|infinitusgo|vigil|#).*/)) {
+        if (!link.attribs.href.match(/.*(resume|google|infinitusgo|vigil|twitter|#).*/)) {
           urlExists(link.attribs.href, (err, exists) => {
             if (err) throw err;
             assert.equal(true, exists, `${link.attribs.href} no longer exists`);
