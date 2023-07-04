f4394b35891b286c01f2d1084a16fcf5c096ced6
Felipe Tuyama <11530478+ftuyama@users.noreply.github.com>
2023-07-04 19:24:40 -0300

    Remove extra style

--- public/js/google-maps.js
+++ public/js/google-maps.js
@@ -275,24 +275,14 @@ $(document).ready(function() {
 function initGoogleMaps() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 13,
-
-        // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(-23.1944292, -45.8821124),
         scrollwheel: false,
-
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
         styles: STYLES
     };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById("map");
-
     // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     // Let"s also add a marker while we"re at it
     var marker = new google.maps.Marker({
