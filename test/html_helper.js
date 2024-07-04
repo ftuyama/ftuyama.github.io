c38d886f9aff6dd68f526ab5507c80ce2ec8def7
Felipe Tuyama <11530478+ftuyama@users.noreply.github.com>
2024-07-04 14:48:23 -0300

    Update dependencies

--- test/html_helper.js
+++ test/html_helper.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const Crawler = require('crawler');
 
-function loadPage(callback) {
+async function loadPage(callback) {
+  const { default: Crawler } = await import('crawler');
+
   fs.readFile('index.html', 'utf8', (err, page) => {
     if (err) throw err;
     new Crawler().queue([{
       html: page,
       callback(error, res) {
-        // eslint-disable-next-line no-console
         if (error) console.log(error);
         callback(page, res.$);
       },
