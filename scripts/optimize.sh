bd933b91771bfebe647b5d8ac0eb8856b2dcc019
Felipe Tuyama <11530478+ftuyama@users.noreply.github.com>
2026-04-13 22:30:16 -0300

    Display header on mobile

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,7 +1,7 @@
 # `for file in static/public/images/**/*; do cwebp -q 80 "$file" -o "${file%.*}.webp"; done`
 # `for file in static/public/images/*; do cwebp -q 80 "$file" -o "${file%.*}.webp"; done`
 
-precision=(static/public/images/header.jpg)
+precision=(static/public/images/header-mobile.png)
 
 for file in "${precision[@]}"; do
     cwebp -q 100 "$file" -o "${file%.*}.webp";
